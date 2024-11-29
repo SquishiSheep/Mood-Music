8b5d128fee1c0a90cda2f0b57147c5222397c0d6
diego <82551829+SquishiSheep@users.noreply.github.com>
2024-11-29 02:40:14 -0600

    mood detector updates

--- Shared/MoodDetector.swift
+++ Shared/MoodDetector.swift
@@ -12,31 +12,31 @@ class MoodDetector {
     }
 
     func startBiometricQuery(completion: @escaping (Double, Double) -> Void) {
-    guard let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate),
-          let hrvType = HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else { return }
+        guard let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate),
+            let hrvType = HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else { return }
 
-    let query = HKSampleQuery(
-        sampleType: heartRateType,
-        predicate: nil,
-        limit: 1,
-        sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)]
-    ) { _, samples, _ in
-        guard let heartRateSample = samples?.first as? HKQuantitySample else { return }
-        let heartRate = heartRateSample.quantity.doubleValue(for: HKUnit(from: "count/min"))
-
-        let hrvQuery = HKSampleQuery(
-            sampleType: hrvType,
+        let query = HKSampleQuery(
+            sampleType: heartRateType,
             predicate: nil,
             limit: 1,
             sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)]
         ) { _, samples, _ in
-            guard let hrvSample = samples?.first as? HKQuantitySample else { return }
-            let hrv = hrvSample.quantity.doubleValue(for: HKUnit.secondUnit(with: .milli))
-            completion(heartRate, hrv)
+            guard let heartRateSample = samples?.first as? HKQuantitySample else { return }
+            let heartRate = heartRateSample.quantity.doubleValue(for: HKUnit(from: "count/min"))
+
+            let hrvQuery = HKSampleQuery(
+                sampleType: hrvType,
+                predicate: nil,
+                limit: 1,
+                sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)]
+            ) { _, samples, _ in
+                guard let hrvSample = samples?.first as? HKQuantitySample else { return }
+                let hrv = hrvSample.quantity.doubleValue(for: HKUnit.secondUnit(with: .milli))
+                completion(heartRate, hrv)
+            }
+            self.healthStore.execute(hrvQuery)
         }
-        self.healthStore.execute(hrvQuery)
+        healthStore.execute(query)
     }
-    healthStore.execute(query)
-}
 
 }
